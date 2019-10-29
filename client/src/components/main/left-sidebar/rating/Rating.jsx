5db8f59a7edac40949fc3baf538a048a4b0a8db4
Falk <petruk0908@gmail.com>
2019-10-30 00:20:02 +0300

    add rating component

---
+++ client/src/components/main/left-sidebar/rating/Rating.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import "./Rating.scss";
+
+const ratings = [
+    {
+        url:'http://top100.rambler.ru/top100/',
+        img: '/img/rambler.gif'
+    },
+    {
+        url:'http://top.list.ru/jump?from=139176',
+        img: '/img/mail.gif'
+    }
+    
+]
+
+export default class Rating extends Component {
+    render() {
+        return (
+            <div>
+                {ratings.map((link) =>
+                    <a href={link.url}>
+                        <img src={link.img} alt="" />
+                    </a>
+                )}
+            </div>
+        )
+    }
+}
