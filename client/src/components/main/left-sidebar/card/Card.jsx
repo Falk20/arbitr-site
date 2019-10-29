b9c33c116be898d2bf5058e525af668782239ad5
Falk <petruk0908@gmail.com>
2019-10-29 23:10:22 +0300

    add card component

---
+++ client/src/components/main/left-sidebar/card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.scss';
+
+export default class Card extends React.Component {
+    render(){
+        return (
+            <div className="aside-card">
+                <a href="#">ФЕДЕРАЛЬНЫЕ АРБИТРАЖНЫЕ СУДЫ РОССИЙСКОЙ ФЕДЕРАЦИИ</a>
+                <embed src="/img/new_map1.swf" type=""/>
+            </div>
+        )
+    }
+}
