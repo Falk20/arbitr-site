2943c686b34712f628cc5b48139aa7ae5f8c9a43
Falk <petruk0908@gmail.com>
2019-10-29 22:39:36 +0300

    finish header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.scss';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <img src="/img/gerb2.gif" alt="" />
+                <h1>федеральные арбитражные суды российской федерации</h1>
+            </header>
+        )
+    }
+}
+
+export default Header;
