4c882fcce9595e0f3a69fdfeafd609dd1ace92cb
Falk <petruk0908@gmail.com>
2019-10-30 18:23:55 +0300

    add main scss

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './Main.scss';
 import LeftSidebar from './left-sidebar/LeftSidebar';
+import Content from './content/Content';
 
 export default class Main extends Component {
     render() {
         return (
             <main>
                 <LeftSidebar></LeftSidebar>
+                <Content></Content>
             </main>
         )
     }
