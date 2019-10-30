c8537096804b6ad3aa49156047aaef96cd8552f1
Falk <petruk0908@gmail.com>
2019-10-30 18:25:33 +0300

    add justice-search component

---
+++ client/src/components/main/content/justice-info/justice-search/JusticeSearch.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './JusticeSearch.scss';
+
+export default class JusticeSearch extends Component {
+    render() {
+        return (
+            <select className="justice-search">
+                <option value="1">Один</option>
+            </select>
+        )
+    }
+}
