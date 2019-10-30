f6a6efc9ac386543a4e585e75ec22170b181cc0e
Falk <petruk0908@gmail.com>
2019-10-30 18:25:16 +0300

    add justice0list component

---
+++ client/src/components/main/content/justice-info/justice-list/JusticeList.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './JusticeList.scss';
+
+const justiceArr = [
+    {
+        link: '/okrug/',
+        text: 'Арбитражные суды округов'
+    },
+    {
+        link: '/appeal/',
+        text: 'Арбитражные апелляционные суды'
+    },
+    {
+        link: '/subj/',
+        text: 'Арбитражные суды субъектов РФ'
+    },
+    {
+        link: '/sip/',
+        text: 'Суд по интеллектуальным правам'
+    }
+]
+
+export default class JusticeList extends Component {
+    render() {
+        return (
+            <ul className='justice-list'>
+                {justiceArr.map((justice)=>
+                    <li><a href={justice.link}>{justice.text}</a></li>
+                )}
+            </ul>
+        )
+    }
+}
