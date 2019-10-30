e8b72c3e26ec0fd6576e89f67589031f31342ad7
Falk <petruk0908@gmail.com>
2019-10-30 18:24:55 +0300

    add info component

---
+++ client/src/components/main/content/justice-info/info/Info.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './info.scss';
+
+export default class Info extends Component {
+    render() {
+        return (
+            <div>
+                <img src="/img/vas.gif" alt="Карта"/>
+
+            </div>
+        )
+    }
+}
