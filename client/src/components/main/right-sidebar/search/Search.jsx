ff13103467073b21e83b5533ba52af54a2eed942
Falk <petruk0908@gmail.com>
2019-11-01 19:02:18 +0300

    add search logic

--- client/src/components/main/right-sidebar/search/Search.jsx
+++ client/src/components/main/right-sidebar/search/Search.jsx
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import './Search.scss';
 
 export default class Search extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             request: ""
+        }
+    }
+    
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        window.location.replace('/search/?req=' + this.state.request);
+    }
+
+    onInput = (e)=> {
+        this.setState({
+            request: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className="search">
                 <h3>Поиск по сайту</h3>
-                <form >
-                    <input type="search" name="" id="" />
+                <form onSubmit={this.onSubmit}>
+                    <input type="search" name="" id="" onInput={this.onInput}/>
                     <input type="image" src="/img/search.svg" alt="" />
                 </form>
             </div>
