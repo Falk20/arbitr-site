cf9c0a3cf86fb96403f3b092e60961e73fe8f8b3
Falk <petruk0908@gmail.com>
2019-11-01 19:00:30 +0300

    add search-req and press-centr

--- client/src/components/main/content/Content.jsx
+++ client/src/components/main/content/Content.jsx
@@ -2,16 +2,48 @@ import React, { Component } from 'react';
 import Breadcrumbs from './breadcrumbs/Breadcrumbs';
 import PageTitle from './page-title/PageTitle';
 import JusticeInfo from './justice-info/JusticeInfo';
+import SearchRequests from './search-request/SearchRequests';
+import PressCentr from './press-centr/PressCentr';
 
-const title = 'Федеральные арбитражные суды Российской Федерации'
+const titles = [
+    'Федеральные арбитражные суды Российской Федерации',
+    'Поиск',
+    'Пресса об арбитражных судах'
+];
 
 export default class Content extends Component {
     render() {
+        let content;
+
+        switch (window.location.pathname) {
+            case '/search/':
+                content =
+                    <>
+                        <Breadcrumbs title={titles[1]}></Breadcrumbs>
+                        <PageTitle title={titles[1]}></PageTitle>
+                        <SearchRequests></SearchRequests>
+                    </>;
+                break;
+            case "/press-centr/":
+                content =
+                    <>
+                        <Breadcrumbs title={titles[2]}></Breadcrumbs>
+                        <PageTitle title={titles[2]}></PageTitle>
+                        <PressCentr></PressCentr>
+                    </>;
+                break;
+            default:
+                content =
+                    <>
+                        <Breadcrumbs title={titles[0]}></Breadcrumbs>
+                        <PageTitle title={titles[0]}></PageTitle>
+                        <JusticeInfo></JusticeInfo>
+                    </>;
+        }
+
         return (
             <div className="content">
-                <Breadcrumbs title={title}></Breadcrumbs>
-                <PageTitle title={title}></PageTitle>
-                <JusticeInfo></JusticeInfo>
+                {content}
             </div>
         )
     }
