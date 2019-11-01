f9e779622fc52d3d730aa753f217eae7895a85cc
Falk <petruk0908@gmail.com>
2019-11-01 19:01:38 +0300

    add routes

---
+++ server/routes/api.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const Record = require('../models/record');
+const unique = require('../methods/uniqueNews');
+
+router.get('/news', function (req, res, next) {
+    let words = new RegExp(req.query.req, 'i');
+    let newsForSend = [];
+
+    Record.find({ title: words }).then((news) => {
+        newsForSend.push(...news);
+
+        Record.find({ body: words }).then((news) => {
+            newsForSend.push(...news);
+
+            Record.find({ source: words }).then((news) => {
+                newsForSend.push(...news);
+                newsForSend = unique(newsForSend);
+
+                res.send(newsForSend);
+            });
+        });
+    });
+});
+
+router.get('/news/:id', function (req, res, next) {
+    Record.findById(req.params.id).then((news) => {
+        res.send(news);
+    });
+});
+
+router.post('/news', function (req, res, next) {
+    Record.create(req.body).then(function (news) {
+        res.send(news);
+    }).catch(next);
+});
+
+module.exports = router;
