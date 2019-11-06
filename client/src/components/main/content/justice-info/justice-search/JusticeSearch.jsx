5ebe45d5fc5aa1bc6d862d137aa942798a4cc26b
Falk <petruk0908@gmail.com>
2019-11-06 19:31:02 +0300

    add sip on select

--- client/src/components/main/content/justice-info/justice-search/JusticeSearch.jsx
+++ client/src/components/main/content/justice-info/justice-search/JusticeSearch.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './JusticeSearch.scss';
 
 export default class JusticeSearch extends Component {
+    onChange = (e) => {
+        e.preventDefault();
+        window.location = e.target.value;
+    }
+
     render() {
         return (
-            <select className="justice-search">
-                <option value="1">Один</option>
+            <select onChange={this.onChange} className="justice-search">
+                <option value="/">Информация об арбитражных судах</option>
+                <option value="/sip/">Суд по интеллектуальным правам</option>
             </select>
         )
     }
