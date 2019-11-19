2ee72797ffb7225f1823c910a989bb55e3a1d500
Falk <petruk0908@gmail.com>
2019-11-19 14:26:31 +0300

    add response log

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,8 @@ const Record = require('../models/record');
 const unique = require('../methods/uniqueNews');
 
 router.get('/news', function (req, res, next) {
+    console.log(req.query.req);
+    
     let words = new RegExp(req.query.req, 'i');
     let newsForSend = [];
 
@@ -20,13 +22,13 @@ router.get('/news', function (req, res, next) {
                 res.send(newsForSend);
             });
         });
-    });
+    }).catch(next);
 });
 
 router.get('/news/:id', function (req, res, next) {
     Record.findById(req.params.id).then((news) => {
         res.send(news);
-    });
+    }).catch(next);
 });
 
 router.post('/news', function (req, res, next) {
