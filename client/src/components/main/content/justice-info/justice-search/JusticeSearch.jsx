d9880a4477f864c4ac93101b6e5e517cad70b1e8
Falk <petruk0908@gmail.com>
2019-11-19 19:13:45 +0300

    add new options

--- client/src/components/main/content/justice-info/justice-search/JusticeSearch.jsx
+++ client/src/components/main/content/justice-info/justice-search/JusticeSearch.jsx
@@ -12,6 +12,9 @@ export default class JusticeSearch extends Component {
             <select onChange={this.onChange} className="justice-search">
                 <option value="/">Информация об арбитражных судах</option>
                 <option value="/sip/">Суд по интеллектуальным правам</option>
+                <option value="/okrug/">Арбитражные суды округов</option>
+                <option value="/appeal/">Арбитражные аппеляционные суды</option>
+                <option value="/subj/">Арбитражные суды субъектов РФ</option>
             </select>
         )
     }
