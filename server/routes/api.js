404aa7da388703393b75a6a170389b7b79cf9cc4
Falk <petruk0908@gmail.com>
2019-11-19 19:14:23 +0300

    fix routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Record = require('../models/record');
 const unique = require('../methods/uniqueNews');
+const JusticeList = require('../models/justiceList');
 
 router.get('/news', function (req, res, next) {
     console.log(req.query.req);
-    
+
     let words = new RegExp(req.query.req, 'i');
     let newsForSend = [];
 
@@ -34,6 +35,20 @@ router.get('/news/:id', function (req, res, next) {
 router.post('/news', function (req, res, next) {
     Record.create(req.body).then(function (news) {
         res.send(news);
+    }).catch(next);
+});
+
+router.get('/justice', function(req, res, next){
+    console.log(req.query.title);
+    JusticeList.findOne({title: req.query.title}).then((list)=>{
+        res.send(list);
+    }).catch(next);
+});
+
+router.post('/justice', function (req, res, next) {
+    console.log(req);
+    JusticeList.create(req.body).then(function (list) {
+        res.send(list);
     }).catch(next);
 });
 
